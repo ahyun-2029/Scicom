27c04613d5882c27ac61e9d9360b75141f8d8c8c
Yun Heejin <ahyun@willamette.edu>
2025-09-15 12:32:11 -0700

    This was the original code for the tax problem.

--- newtax.py
+++ newtax.py
@@ -1,15 +1,21 @@
 def single_tax(pay):
-    tax_policy = [
-        [415050, .396], 
-        [413350, .35],
-        [190150, .33],
-        [91150, .28],
-        [37650, .25],
-        [9275, .15]
-    ]
     tax = 0
-    for bracket in tax_policy:
-        if pay > bracket[0]:
-            tax += (pay - bracket[0]) * bracket[1]
-            pay = bracket[0]
+    if pay > 415050:
+        tax += (pay - 415050) * .396
+        pay = 415050
+    if pay > 413350:
+        tax += (pay - 413350) * .35
+        pay = 413350
+    if pay > 190150:
+        tax += (pay - 190150) * .33
+        pay = 190150
+    if pay > 91150:
+        tax += (pay - 91150) * .28
+        pay = 91150
+    if pay > 37650:
+        tax += (pay - 37650) * .25
+        pay = 37650
+    if pay > 9275:
+        tax += (pay - 9275) * .15
+        pay = 9275
     return tax + pay * .1
